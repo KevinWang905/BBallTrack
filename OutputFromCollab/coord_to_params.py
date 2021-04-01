d8868fffd424b26262234019d6ff76455bf7cb7b
KevinWang905 <46271360+KevinWang905@users.noreply.github.com>
2021-04-01 16:34:44 -0400

    Add files via upload

--- OutputFromCollab/coord_to_params.py
+++ OutputFromCollab/coord_to_params.py
@@ -9,18 +9,21 @@
 
 import pandas as pd
 import math
-
+from measurements import *
 
 # import data and remove null rows
 rawData = pd.read_csv('coord_df.csv')
 rawData.dropna(axis = 0,inplace= True)
 rawData.reset_index(inplace = True)
 
-
  
 def getAngle(a, b, c):
     ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
     return ang + 360 if ang < 0 else ang
+
+def getDistance(a,b):
+	distance = ((((b[0] - a[0] )**2) + ((b[1]-a[1])**2) )**0.5)
+	return distance
 
 def shoulder_flexion(frame):
 	elbow_coords = (rawData.loc[frame,'Elbow_R_X'], rawData.loc[frame,'Elbow_R_Y'])
@@ -50,13 +53,40 @@ def trunk_flexion(frame):
 	angle = getAngle(chest_coords, groin_coords, knee_coords)
 	return 360 - angle if angle > 180 else angle
 
+def wrist_flexion(frame):
+	elbow_coords = (rawData.loc[frame,'Elbow_R_X'], rawData.loc[frame,'Elbow_R_Y'])
+	wrist_coords = (rawData.loc[frame,'Wrist_R_X'], rawData.loc[frame,'Wrist_R_Y'])
+	knuckle_coords = (rawData.loc[frame,'Mid_knuckle_X'], rawData.loc[frame,'Mid_knuckle_Y'])
+	angle = getAngle(elbow_coords, wrist_coords, knuckle_coords)
+	return angle
+
+def finger_flexion(frame):
+	finger_coords = (rawData.loc[frame,'Mid_fing_X'], rawData.loc[frame,'Mid_fing_Y'])
+	knuckle_coords = (rawData.loc[frame,'Mid_knuckle_X'], rawData.loc[frame,'Mid_knuckle_Y'])
+	wrist_coords = (rawData.loc[frame,'Wrist_R_X'], rawData.loc[frame,'Wrist_R_Y'])
+	angle = getAngle(finger_coords, knuckle_coords, wrist_coords)
+	return angle
+
+def finger_flexion(frame):
+	finger_coords = (rawData.loc[frame,'Mid_fing_X'], rawData.loc[frame,'Mid_fing_Y'])
+	knuckle_coords = (rawData.loc[frame,'Mid_knuckle_X'], rawData.loc[frame,'Mid_knuckle_Y'])
+	wrist_coords = (rawData.loc[frame,'Wrist_R_X'], rawData.loc[frame,'Wrist_R_Y'])
+	angle = getAngle(finger_coords, knuckle_coords, wrist_coords)
+	return angle
+
+def ball_shoulder(frame):
+	ball_coords = (rawData.loc[frame,'Wrist_R_X'], rawData.loc[frame,'Wrist_R_Y'])
+	shoulder_coords = (rawData.loc[frame,'Shoulder_R_X'], rawData.loc[frame,'Shoulder_R_Y'])
+	distance = getDistance(ball_coords, shoulder_coords)
+	return distance
+
 def get_parameters():
 	param_list = [] 
 	for i in range(len(rawData)):
-		param_list = param_list + [[i,shoulder_flexion(i),elbow_flexion(i),knee_flexion(i),trunk_flexion(i)]]
-	params = pd.DataFrame(param_list, columns = ['frame', 'shoulder_flexion', 'elbow_flexion', 'knee_flexion', 'trunk flexion'])
+		param_list = param_list + [[i,shoulder_flexion(i),elbow_flexion(i),knee_flexion(i),trunk_flexion(i), wrist_flexion(i), finger_flexion(i), ball_shoulder(i), rawData.loc[i,'Wrist_R_Y']]]
+	params = pd.DataFrame(param_list, columns = ['frame', 'shoulder_flexion', 'elbow_flexion', 'knee_flexion', 'trunk flexion', 'wrist_flexion', 'finger_flexion', 'ball_from_body','ball_height'])
 	params.to_csv("params_df.csv")
 	return 0
 
-
+print(Shooters[DAME].height)
 get_parameters()
